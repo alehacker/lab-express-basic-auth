fef90d416a2973dd5677afbc6e9d7784c28bf375
Ale Kennedy <alepertuz@gmail.com>
2023-02-09 14:44:32 -0500

    Iteration 2 Done

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -31,11 +31,41 @@ router.post('/signup', (req, res, next) =>{
       })
     .catch((err) => {
         console.log(err)
-    })
-    // res.redirect('/users/user-profile')  
-
+    }) 
 })
 
 
+// Login Routes
+router.get('/login', (req, res, next) => {
+    res.render('auth/login.hbs')
+})
+
+router.post('/login', (req, res, next) => {
+    const { username, password } = req.body;
+   
+    if (!username || !password) {
+      res.render('auth/login.hbs', {
+        errorMessage: 'Please enter both, username and password to login.'
+      });
+      return;
+    }
+   
+    User.findOne({ 
+        username: req.body.username
+    })
+      .then(user => {
+        if (!user) {
+          res.render('auth/login.hbs', { errorMessage: 'Email is not registered. Try with other email.' });
+          return;
+        } else if (bcryptjs.compareSync(password, user.passwordHash)) {
+          req.session.user = user
+          console.log('SESSION =====> ', req.session);
+          res.send('Login Success - Session Started');
+        } else {
+          res.render('auth/login', { errorMessage: 'Incorrect password.' });
+        }
+      })
+      .catch(error => next(error));
+  });
 
 module.exports = router;
